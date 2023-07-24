0f951e533eed78c61a71d7e0e1bf34ea29ff0dd2
Dilip Kumar <curiousdilip@gmail.com>
2023-07-24 15:53:24 +0530

    tried to active class

--- app/components/header.js
+++ app/components/header.js
@@ -5,10 +5,29 @@ import { useRouter } from 'next/navigation';
 import logoDark from '../../public/assets/img/tb-logo.svg';
 import logoLight from '../../public/assets/img/tb-logo-black.svg';
 import './header.css';
-
+import React, { useEffect, useState } from 'react';
 
 const CustomNavbar = ({ theme }) => {
     const router = useRouter();
+    const [activePath, setActivePath] = useState('/');
+
+    useEffect(() => {
+        // Function to handle route change
+        const handleRouteChange = (url) => {
+            setActivePath(url);
+        };
+
+        // Check if router.events exists before adding the event listener
+        if (router?.events) {
+            // Listen to the 'routeChangeComplete' event to update the active path
+            router.events.on('routeChangeComplete', handleRouteChange);
+
+            // Clean up the event listener when the component unmounts
+            return () => {
+                router.events.off('routeChangeComplete', handleRouteChange);
+            };
+        }
+    }, [router]);
 
     const navbarClass = classnames('navbar', {
         'navbar-dark': theme === 'dark',
@@ -31,13 +50,14 @@ const CustomNavbar = ({ theme }) => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/work" className={router.pathname === '/work' ? 'active' : ''}>Our Work</Nav.Link>
-                        <Nav.Link href="/services" className={router.pathname === '/services' ? 'active' : ''}>Services</Nav.Link>
+                        <Nav.Link href="/work" className={activePath === '/work' ? 'active' : ''}>Our Work</Nav.Link>
+                        <Nav.Link href="/services" className={activePath === '/services' ? 'active' : ''}>Services</Nav.Link>
                     </Nav>
                     <Button variant="primary">
-                        <Nav.Link href="/contact" className={router.pathname === '/contact' ? 'active' : ''}>Contact</Nav.Link>
+                        <Nav.Link href="/contact" className={activePath === '/contact' ? 'active' : ''}>Contact</Nav.Link>
                     </Button>
-                </Navbar.Collapse></Container>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 };
