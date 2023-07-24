aa34b157885bdb320feff43b9e00d36b10fd92c0
Dilip Kumar <curiousdilip@gmail.com>
2023-07-24 12:21:32 +0530

    login working now

---
+++ app/login/page.js
@@ -0,0 +1,82 @@
+
+"use client";
+import { useState } from 'react';
+import { login } from '../api/login'
+import supabase from "../supabase";
+import "./login.css";
+import { useRouter } from 'next/navigation'; // Import useRouter
+import Image from 'next/image';
+import logo from '../../public/assets/img/tb-logo-black.svg'
+import React from 'react'
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter(); // Initialize the useRouter hook
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { user, error } = await supabase.auth.signInWithPassword({
+                email,
+                password,
+            });
+
+            if (error) {
+                throw new Error(error.message);
+            }
+
+            // Handle successful login, e.g., redirect to dashboard
+            console.log('Logged in user:', user);
+            router.push('/countries');
+        } catch (error) {
+            console.error('Login error:', error.message);
+        }
+    };
+    return (
+        <>
+            <section id="login">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="login-form">
+                                <Image
+                                    src={logo}
+                                    alt="Logo"
+                                    width="250"
+                                    height="61"
+                                />
+                                <form action="#">
+                                    <input
+                                        className="form-control"
+                                        id="form1"
+                                        type="email"
+                                        placeholder="Email Address"
+                                        // onChange={updateEmail}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                    <input
+                                        className="form-control"
+                                        id="form2"
+                                        placeholder="Password"
+                                        // onChange={updatePassword}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        type="password"
+                                    />
+                                    <button className="login-btn "
+                                        onClick={(e) => handleLogin(e)}
+                                    >
+                                        Sign in
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Login
--- app/supabase.js
+++ app/supabase.js
@@ -1,8 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
+const storage = typeof window !== 'undefined' ? window.localStorage : null;
 
-const supabaseUrl = 'https://anyijxohgwdjjomafoml.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFueWlqeG9oZ3dkampvbWFmb21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk3ODY5MjIsImV4cCI6MjAwNTM2MjkyMn0.0q5dBxDBvQwFoxuoa4yg1S8wWtOT3uF1sK-DZfRZtLE';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    localStorage: storage, // Use localStorage for session persistence
+});
 
 export default supabase;
